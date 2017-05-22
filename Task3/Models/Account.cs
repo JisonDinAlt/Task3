1a835b15eb4d1efbdfed01836881609585bea9fa
esarvas <esarvas@WS-SARVAS>
2017-05-22 08:54:45 +0300

    create

---
+++ Task3/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Models
+{
+    public class Account
+    {
+        public string name { get; protected set; }
+        public int number { get; protected set; }
+        public string tariff { get; protected set; }
+
+
+    }
+}
---
+++ Task3/Models/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task3.Interfaces;
+
+namespace Task3.Models
+{
+    class Number : INumber
+    {
+        public int number { get;  set; }
+    }
+}
