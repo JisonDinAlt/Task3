f766a3b77b996d3e08bb7ed89bdafae716fe2c54
esarvas <esarvas@WS-SARVAS>
2017-05-23 18:46:22 +0300

    upd tariff

--- Task3/Models/Tariff.cs
+++ Task3/Models/Tariff.cs
@@ -8,15 +8,28 @@ namespace Task3.Models
 {
     class Tariff
     {
+        //тарифы
+        //    1. Unlim - Безлимит, стоимость 1000 в месяц 
+        //    2. Fre100 - 100 бесплатных минут, остальные по 10 руб мин, цена 500
+        //    3. Free50 - 50 бесплатных минут, остальные по 10 руб мин, цена 250
+        //    4. Free10 - 10 бесплатных минут, остальные по 10 руб мин, цена 100
+        //    5. Basic -  0 бесплатных, тарификация 20 руб мин, цена 0
+
+
         public string name { get; }
         public int id { get; }
-        public string description { get; }
-       
-        public Tariff (string _name, int _id, string _description)
+        public bool isUnlim { get; }
+        public double freeMinuts { get; set; }
+        public double cost { get; }
+         
+        
+
+
+        public Tariff (string _name, int _id)
         {
             _name = name;
             _id = id;
-            _description = description;
+           
         }
     }
 }
